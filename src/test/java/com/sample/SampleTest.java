9854db93335c5d0780cf7f6cb12d090c3833ae89
Tamilselvan1101 <tamilantony1999@gmail.com>
2021-08-11 00:47:50 +0530

    Test1

---
+++ src/test/java/com/sample/SampleTest.java
@@ -0,0 +1,38 @@
+package com.sample;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	private void tc01() {
+		System.out.println("Method 1");
+
+	}
+
+	@Test
+	private void tc02() {
+		System.out.println("Method 2");
+
+	}
+
+	@Test
+	private void tc03() {
+		System.out.println("Method 3");
+
+	}@Test
+	private void tc04() {
+		System.out.println("Method 4");
+
+	}@Test
+	private void tc05() {
+		System.out.println("Method 5");
+
+	}@Test
+	private void tc06() {
+		System.out.println("Method 6");
+
+	}
+	
+	
+}
